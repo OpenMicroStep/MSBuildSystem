7e59a5117f1f7bf27696c44cdf1ffde9abb47d42
Vincent Rouillé <v-rouille@logitud.fr>
2017-01-23 15:49:14 +0100

    ci: add publish

--- @msbuildsystem/make.js
+++ @msbuildsystem/make.js
@@ -35,7 +35,7 @@ module.exports= {
     compiler: "typescript",
     environments: ["=node"],
     npmPackage: [{
-      "version": "0.1.7",
+      "version": "0.1.8",
       "main": "index.js",
       "typings": "index.d.ts"
     }],
