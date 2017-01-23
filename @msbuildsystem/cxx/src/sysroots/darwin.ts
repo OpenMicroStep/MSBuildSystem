fde93e3ba2301cab9480f4d5fef9a3146394d9c7
Vincent Rouillé <v-rouille@logitud.fr>
2017-01-23 12:37:52 +0100

    cxx: add missing files

---
+++ @msbuildsystem/cxx/src/sysroots/darwin.ts
@@ -0,0 +1,86 @@
+import {Reporter, File, AttributeTypes, resolver} from '@msbuildsystem/core';
+import {ProcessTask} from '@msbuildsystem/foundation';
+import {
+  CompilerOptions, LinkerOptions, Conditions, CXXSysroots, CXXTarget,
+  CXXSysroot, CXXLibrary, CXXFramework, CXXLinkType,
+  CompileTask, LinkTask, LinkClangTask, LinkLibToolTask, LipoTask, CompileClangTask, CompileGCCTask
+} from '../index.priv';
+
+@CXXSysroots.declare({
+  compilers: {
+    "clang": CompileClangTask,
+    "gcc": CompileGCCTask
+  },
+  ranLinkers: { "libtool": LinkLibToolTask },
+  libLinkers: { "clang": LinkClangTask },
+  exeLinkers: { "clang": LinkClangTask },
+  defaultCompiler: "clang",
+  defaultRanLinker: "libtool",
+  defaultLibLinker: "clang",
+  defaultExeLinker: "clang",
+})
+export class CXXDarwinSysroot extends CXXSysroot {
+  architectures: string[];
+
+  @resolver(AttributeTypes.validateString)
+  osxVersionMin = "10.10";
+
+  constructor(graph: CXXTarget, conditions: Conditions) {
+    super({ type: 'sysroot', name: 'darwin' }, graph, conditions);
+    this.architectures = (this.architecture || "").split(",");
+  }
+
+  static isCompatible(conditions: Conditions) : boolean {
+    return conditions.platform === "darwin";
+  }
+
+  createTasks(reporter: Reporter) {
+    if (this.architectures.length <= 1)
+      return super.createTasks(reporter);
+
+    let ret = { compileTasks: <CompileTask[]>[], linkTasks: <LinkTask[]>[] };
+    for (let arch of this.architectures) {
+      this.architecture = arch;
+      let sub = super.createTasks(reporter);
+      ret.compileTasks.push(...sub.compileTasks);
+      ret.linkTasks.push(...sub.linkTasks);
+    }
+    this.architecture = "";
+    let lipoTask = new LipoTask(this.graph, ret.linkTasks, File.getShared(this.linkFinalPath()));
+    ret.linkTasks = [lipoTask];
+    return ret;
+  }
+
+  configureCompileTask(reporter: Reporter, task: CompileTask, params: CompilerOptions) {
+    this._addFlags(task);
+    super.configureCompileTask(reporter, task, params);
+  }
+
+  configureLinkTask(reporter: Reporter, task: LinkTask, params: LinkerOptions) {
+    this._addFlags(task);
+    super.configureLinkTask(reporter, task, params);
+  }
+
+  _addFlags(task: ProcessTask) {
+    if (task instanceof LinkLibToolTask) {
+      if (this.graph.linkType !== CXXLinkType.STATIC)
+        task.addFlags([`-compatibility_version`, this.osxVersionMin]);
+    }
+    else
+      task.addFlags([`-mmacosx-version-min=${this.osxVersionMin}`]);
+    if (this.graph.linkType === CXXLinkType.DYNAMIC)
+      task.addFlags(["-fPIC"]);
+    // if (this.triples && target.linkType !== CXXTarget.LinkType.STATIC)
+    //   task.addFlags(["--target=" + this.triples[target.arch]]);
+  }
+
+  linkFinalName() : string {
+    let name = super.linkFinalName();
+    if (this.graph instanceof CXXLibrary && !(this.graph instanceof CXXFramework)) {
+        name = "lib" + name;
+        name += (this.architectures.length > 1 && this.architecture ? "." + this.architecture : "");
+        name += (this.graph.linkType === CXXLinkType.DYNAMIC ? ".dylib" : ".a");
+    }
+    return name;
+  }
+}
---
+++ @msbuildsystem/cxx/src/sysroots/linux.ts
@@ -0,0 +1,98 @@
+import {Reporter, SubGraph, File, Graph} from '@msbuildsystem/core';
+import {ProcessTask} from '@msbuildsystem/foundation';
+import {CompileFileParams, CXXLinkType} from '../target';
+import {CXXSysroot, Conditions} from '../sysroot';
+import {CompileTask} from '../tasks/compile';
+import {LinkTask} from '../tasks/link';
+import {LinkBinUtilsTask} from '../tasks/linkBinUtils';
+import {CompileClangTask} from '../tasks/compileClang';
+import {CompileGCCTask} from '../tasks/compileGCC';
+import {CXXLibrary} from '../targets/library';
+import {CXXFramework} from '../targets/framework';
+
+@declareSysroot({
+  compilers: {
+    "clang": CompileClangTask,
+    "gcc": CompileGCCTask
+  },
+  linkers: {
+    "binutils": LinkBinUtilsTask
+  },
+  defaultCompiler: "clang",
+  defaultLinker: "clang"
+})
+class LinuxSysroot extends CXXSysroot {
+
+  createCompileTask(reporter: Reporter, graph: Graph, srcFile: File, objFile: File, params: CompileFileParams) : CompileTask {
+    let task = super.createCompileTask(reporter, graph, srcFile, objFile, params);
+    if (this.target.linkType !== CXXLinkType.EXECUTABLE)
+      task.addFlags(["-fPIC"]);
+    // if (this.triples && target.linkType !== CXXTarget.LinkType.STATIC)
+    //   task.addFlags(["--target=" + this.triples[target.arch]]);
+    //   task.addFlags(["--sysroot=" + this.sysrootDirectory]);
+    return task;
+  }
+
+  createLinkTask(reporter: Reporter, graph: Graph) : LinkTask {
+    let task = super.createLinkTask(reporter, graph);
+    this._addFlags(task);
+    return task;
+  }
+
+  linkTaskProvider(linkerName: string) {
+    let provider;
+    if (target.linkType === CXXTarget.LinkType.STATIC)
+      conditions = {archiver:"binutils", triple:this.triple};
+    else
+      conditions = {compiler:"gcc", triple:this.triple};
+  }
+
+  createLinkTask(target: CXXTarget, compileTasks: CompileTask[], finalFile: File, callback: Sysroot.CreateTaskCallback) {
+    var conditions;
+    if (target.linkType === CXXTarget.LinkType.STATIC)
+      conditions = {archiver:"binutils", triple:this.triple};
+    else
+      conditions = {compiler:"gcc", triple:this.triple};
+
+    var task = new LinkBinUtilsTask(target, compileTasks, finalFile, target.linkType, conditions);
+    if (target.linkType !== CXXTarget.LinkType.STATIC)
+      task.addFlags(["-Wl,-soname," + this.linkFinalName(target)]);
+    var basepath= path.dirname(finalFile.path);
+    var rpaths = [];
+    var rpathslnk = [];
+    var exported = new Set<Target>();
+    var deep = (parent: Target) => {
+      parent.dependencies.forEach((dep) => {
+        if (!exported.has(dep)) {
+          exported.add(dep);
+          rpaths.push(path.relative(basepath, path.dirname(this.linkFinalPath(<CXXTarget>dep))));
+          rpathslnk.push(path.dirname(this.linkFinalPath(<CXXTarget>dep)));
+          deep(dep);
+        }
+      });
+    };
+    deep(target);
+    rpathslnk= _.unique(rpathslnk);
+    rpaths= _.unique(rpaths);
+    task.addFlags(rpaths.map((p) => { return "-Wl,-rpath,$ORIGIN/" + p + "/"; }));
+    task.addFlags(rpathslnk.map((p) => { return "-Wl,-rpath-link," + p; }));
+    task.addFlags(["-Wl,-rpath-link," + path.join(this.sysrootDirectory, 'usr/lib/x86_64-linux-gnu')]);
+    task.addFlags(["-Wl,-rpath-link," + path.join(this.sysrootDirectory, 'lib/x86_64-linux-gnu')]);
+    if (this.triple) {
+      task.addFlags(["--target=" + this.triple]);
+      task.addFlags(["--sysroot=" + this.sysrootDirectory]);
+    }
+    callback(null, task);
+  }
+
+  linkFinalName() : string {
+    let name = super.linkFinalName();
+    if (this.target instanceof CXXLibrary)
+        name = "lib" + name +(this.target.linkType === CXXLinkType.DYNAMIC ? ".so" : ".a");
+    return name;
+  }
+}
+
+LinuxSysroot.prototype.platform = "win32";
+
+export = LinuxSysroot;
