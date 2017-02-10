18c0cee629d07f60a3f223670bccb5aa2c373170
Vincent Rouillé <v-rouille@logitud.fr>
2017-02-10 15:14:24 +0100

    project: remove old useless files

--- _/test-buildsystem.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const core = require('./bootstrap6/node/debug/node_modules/@msbuildsystem/core');
-core.Loader.loadModules();
-console.info("Loaded modules: ", Array.from(core.Loader.modules.values()).map(m => m.name).join(', ')),
-core.Async.run(null, (f) => { core.Loader.testModules(f); });
