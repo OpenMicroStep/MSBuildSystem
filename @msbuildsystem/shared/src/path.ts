2c58a59636b6c8b1a01b5d4bd8bedb1053a3ed88
Vincent Rouillé <v-rouille@logitud.fr>
2017-03-09 15:12:03 +0100

    shared: path typo

--- @msbuildsystem/shared/src/path.ts
+++ @msbuildsystem/shared/src/path.ts
@@ -1,11 +1,13 @@
 
 export function basename(path: string) : string {
-  var idx = path.length, c;
-  while (idx > 0 && (c= path[--idx]) !== '/' && c !== '\\');
+  let idx = path.length;
+  let c: string | undefined;
+  while (idx > 0 && (c = path[--idx]) !== '/' && c !== '\\');
   return (c === '/' || c === '\\') ? path.substring(idx + 1) : path;
 }
 export function extname(path: string) : string {
-  var idx = path.length, c;
+  let idx = path.length;
+  let c: string | undefined;
   while (idx > 1 && (c= path[--idx]) !== '.' && c !== '/' && c !== '\\');
   return idx > 0 && c === '.' ? path.substring(idx) : '';
 }
