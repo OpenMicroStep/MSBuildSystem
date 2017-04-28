54460a6f354a7f952ca2769c72e4fe3442f2b99b
Vincent Rouillé <v-rouille@logitud.fr>
2017-04-28 18:07:28 +0200

    core: injection tests for spec changes

--- @msbuildsystem/core/tst/injection.spec.ts
+++ @msbuildsystem/core/tst/injection.spec.ts
@@ -11,30 +11,52 @@ function mock_buildtarget() : BuildTargetElement {
 }
 
 function mock_element(obj: object, id: string | number) : Element {
-  obj["__path"] = () => `E${id}`;
-  return obj as Element;
+  let ret = new Element('element', `E${id}`, null);
+  Object.assign(ret, obj);
+  return ret;
 }
 
-function mock_component(obj: object, id: string | number) : ComponentElement {
-  obj = mock_element(obj, id);
-  obj["components"] = obj["components"] || [];
-  return obj as ComponentElement;
+function mock_component(obj: object, name: string) : ComponentElement {
+  let ret = new ComponentElement('component', name, null);
+  Object.assign(ret, obj);
+  return ret;
 }
 
+function clean(a) {
+  if (a && typeof a === "object") {
+    delete a["components"];
+    delete a["componentsByEnvironment"];
+    delete a["is"];
+    delete a["name"];
+    delete a["tags"];
+    for (let k in a)
+      a[k] = clean(a[k]);
+  }
+  return a;
+}
 
 function testInjectElements(into: object, elements: object[], diags: Diagnostic[], expect: object) {
   let reporter = new Reporter();
-  injectElements(reporter, elements.map((e, i) => mock_element(e, i)), into, new AttributePath('I'), mock_buildtarget());
+  let el = mock_element(into, 'I');
+  injectElements(reporter, elements.map((e, i) => mock_element(e, i)), el, new AttributePath('I'), mock_buildtarget());
   assert.deepEqual(reporter.diagnostics, diags);
-  assert.deepEqual(into, expect);
+  assert.deepEqual(clean(el.toJSON()), expect);
 }
 
 function testInjectComponentsOf(into: object, component: ComponentElement, diags: Diagnostic[], expect: object) {
   let reporter = new Reporter();
-  injectComponentsOf(reporter, component, into, new AttributePath('I'), mock_buildtarget());
+  let el = mock_element(into, 'I');
+  injectComponentsOf(reporter, component, el, new AttributePath('I'), mock_buildtarget());
   assert.deepEqual(reporter.diagnostics, diags);
-  delete into["components"];
-  assert.deepEqual(into, expect);
+  assert.deepEqual(clean(el.toJSON()), expect);
+}
+
+function testInjectComponents(into: object, components: ComponentElement[], diags: Diagnostic[], expect: object) {
+  let reporter = new Reporter();
+  let el = mock_element(into, 'I');
+  injectComponentsOf(reporter, { components: components }, el, new AttributePath('I'), mock_buildtarget());
+  assert.deepEqual(reporter.diagnostics, diags);
+  assert.deepEqual(clean(el.toJSON()), expect);
 }
 
 function primitive_nocollision() {
@@ -138,7 +160,7 @@ function components_collision() {
       mock_component({ c: 4 }, '01'),
     ],
   }, '0'),
-  [{ "type": "warning", "msg": "collision on I.c: attribute is removed", "path": "E01.c" }],
+  [{ "type": "warning", "msg": "collision on I.c: attribute is removed", "path": "01.c" }],
   { a: 1, b: 2 });
 }
 
@@ -154,6 +176,48 @@ function components_array() {
   }, '0'),
   [],
   { a: 1, b: 2, c: [1, 2, 3, 4] });
+}
+
+function components_subs() {
+  testInjectComponents({
+    a: mock_component({ b: 1 }, "I.a"),
+    l: mock_component({ b: [1] }, "I.l"),
+    r: mock_component({ b: mock_component({ b: [1] }, "I.r.b") }, "I.r"),
+    x: [mock_component({ b: mock_component({ b: [1] }, "I.x0.b") }, "I.x0")],
+  },
+  [mock_component({
+    a: mock_component({ b: 2, c: 3 }, "0.a"),
+    l: mock_component({ b: [2] }, "0.l"),
+    r: mock_component({ b: mock_component({ b: [2] }, "0.r.b") }, "0.r"),
+    x: [mock_component({ b: mock_component({ b: [2] }, "0.x0.b") }, "0.x0")],
+    components: [
+      mock_component({
+        a: mock_component({ b: 3, c: 4, d: 5 }, "00.a"),
+        l: mock_component({ b: [3] }, "00.l"),
+        r: mock_component({ b: mock_component({ b: [3] }, "00.r.b") }, "00.r"),
+        x: [mock_component({ b: mock_component({ b: [3] }, "00.x0.b") }, "00.x0")],
+      }, "00"),
+      mock_component({
+        a: mock_component({ b: 3, c: 4, e: 6 }, "01.a"),
+        l: mock_component({ components: [
+          mock_component({ b: [4] }, "01.l0"),
+        ]}, "01.l"),
+        r: mock_component({ components: [
+          mock_component({ b: mock_component({ b: [4] }, "01.r0.b") }, "01.r0"),
+        ]}, "01.r"),
+        x: [mock_component({ components: [
+          mock_component({ b: mock_component({ b: [4] }, "01.x00.b") }, "01.x00"),
+        ]}, "01.x0")],
+      }, "01"),
+    ]
+  }, '0')],
+  [],
+  {
+    a: { b: 1, c: 3, d: 5, e: 6 },
+    l: { b: [1, 2, 3, 4]},
+    r: { b: { b: [1, 2, 3, 4]} },
+    x: [{ b: { b: [1] } }, { b: { b: [2] } }, { b: { b: [3] } }, { b: { b: [4] } }],
+  });
 }
 
 export const tests = { name: 'injection', tests: [
@@ -172,4 +236,5 @@ export const tests = { name: 'injection', tests: [
   components,
   components_collision,
   components_array,
+  components_subs,
 ] };
