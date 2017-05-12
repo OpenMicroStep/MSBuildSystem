523c73d71190fc3c9954868a0bb721b934415bc7
Vincent (Speedy37) Rouille <vincent@speedy37.fr>
2017-05-13 00:05:27 +0200

    project: raise version number to v0.4.4

--- @msbuildsystem/make.js
+++ @msbuildsystem/make.js
@@ -33,7 +33,7 @@ module.exports= {
     compiler: "typescript",
     environments: ["=node"],
     npmPackage: [{ is: "component",
-      "version": "0.4.2",
+      "version": "0.4.4",
       "main": "index.js",
       "typings": "index.d.ts",
       devDependencies: [{ is: "component",
