bf4b45eeb124f2204db891b806ed6a19921015ba
Vincent Rouillé <v-rouille@logitud.fr>
2017-09-01 17:29:06 +0200

    cli: improve output format & refactor output reporting

--- @msbuildsystem/cli/src/common.ts
+++ @msbuildsystem/cli/src/common.ts
@@ -1,46 +1,60 @@
 import { Reporter, TaskReporter, Diagnostic, util } from '@openmicrostep/msbuildsystem.core';
 import * as chalk from 'chalk';
+import * as fs from 'fs';
 
-export const colors = {
-  "note": chalk.cyan,
-  "remark": chalk.cyan,
-  "warning": chalk.magenta,
-  "error": chalk.red,
-  "fatal error": chalk.red
-};
-
-export type Report = { name: string, diagnostics: Diagnostic[], logs: string | undefined, failed: boolean, duration?: number, stats: Stats };
 export type Stats = {
-    "note": number,
-    "remark": number,
-    "warning": number,
-    "error": number,
-    "fatal error": number,
-  };
-function mkStats() {
-  return {
-    "note": 0,
-    "remark": 0,
-    "warning": 0,
-    "error": 0,
-    "fatal error": 0,
-  };
+  "note": number,
+  "remark": number,
+  "warning": number,
+  "error": number,
+  "fatal error": number,
+};
+function reduceStats(previous, current) {
+  previous["note"]        += current["note"];
+  previous["remark"]      += current["remark"];
+  previous["warning"]     += current["warning"];
+  previous["error"]       += current["error"];
+  previous["fatal error"] += current["fatal error"];
 }
+
+export class Report {
+  constructor(public name: string, reporter?: (Reporter | TaskReporter), duration?: number) {
+    this.diagnostics = reporter ? reporter.diagnostics : [];
+    this.logs = reporter ? (reporter as TaskReporter).logs : undefined;
+    this.failed = reporter ? reporter.failed : false;
+    this.duration = duration;
+    this.stats = {
+      "note": 0,
+      "remark": 0,
+      "warning": 0,
+      "error": 0,
+      "fatal error": 0,
+    };
+    this.diagnostics.reduce((prev, current) => {
+      prev[current.type] += 1;
+      return prev;
+    }, this.stats);
+    if (this.failed && !this.stats.error && !this.stats["fatal error"])
+      this.stats["fatal error"] += 1;
+  }
+
+  diagnostics: Diagnostic[];
+  logs?: string;
+  failed: boolean;
+  duration?: number;
+  stats: Stats;
+}
+
 function indent(indent: string, text: string) {
   return text ? indent + text.replace(/\n/g, `\n${indent}`) : text;
 }
 function embed(prefix: string, text: string, suffix: string) {
   return text ? prefix + text + suffix : text;
 }
-function reduce(diagnostics: Diagnostic[], stats: Stats) {
-  return diagnostics.reduce((prev, current) => {
-    prev[current.type] += 1;
-    return prev;
-  }, stats);
-}
+
 export class ReporterPrinter {
   static types = ["note", "remark", "warning", "error", "fatal error"];
-  static colors = {
+  colors = {
     "note": chalk.cyan,
     "remark": chalk.cyan,
     "warning": chalk.magenta,
@@ -48,44 +62,37 @@ export class ReporterPrinter {
     "fatal error": chalk.red
   };
   totalDiagnosticCount = 0;
-  report: Report = {
-    name: '',
-    diagnostics: [],
-    failed: false,
-    logs: '',
-    stats: mkStats()
-  };
+  report: Report = new Report('global');
   reports: Report[] = [];
   push(report: Report) {
     this.report.diagnostics.push(...report.diagnostics);
     this.report.failed = this.report.failed || report.failed;
-    reduce(report.diagnostics, this.report.stats);
+    reduceStats(this.report.stats, report.stats);
     this.totalDiagnosticCount += report.diagnostics.length;
     this.reports.push(report);
   }
   formatStats() {
-    return ReporterPrinter.formatStats(this.report.stats);
+    let stats = this.report.stats;
+    return ReporterPrinter.types
+      .filter(type => stats[type] > 0)
+      .map(type => `${stats[type]} ${this.colors[type](type + (stats[type] > 1 ? 's' : ''))}`)
+      .join(', ');
   }
   formatReports(name: string, duration?: number) {
     let ret = "";
     for (let report of this.reports) {
       if (report.failed || report.diagnostics.length)
-        ret += `${ReporterPrinter.formatReportConclusion(report)}:\n${embed('', indent('  ', ReporterPrinter.formatReportLogs(report)), '\n')}\n`;
+        ret += `${chalk.grey(this.formatReportConclusion(report))}:\n${this.formatReportLogs(report, '  ')}\n`;
     }
-    ret += ReporterPrinter.formatReportConclusion(Object.assign({}, this.report, { name: name, duration: duration }));
+    ret += this.formatReportConclusion(Object.assign({}, this.report, { name: name, duration: duration }));
     return ret;
   }
-  static hasStats(stats: Stats) {
+  hasStats() {
+    let stats = this.report.stats;
     return ReporterPrinter.types
       .filter(type => stats[type] > 0).length > 0;
   }
-  static formatStats(stats: Stats) {
-    return ReporterPrinter.types
-      .filter(type => stats[type] > 0)
-      .map(type => `${stats[type]} ${ReporterPrinter.colors[type](type + (stats[type] > 1 ? 's' : ''))}`)
-      .join(', ');
-  }
-  static formatDiagnostic(d: Diagnostic, indent = "") {
+  formatDiagnostic(d: Diagnostic, indent = "", showContext = true) {
     let ret = indent;
     if (d.path) {
       ret += d.path;
@@ -96,46 +103,102 @@ export class ReporterPrinter {
       }
       ret += " ";
     }
-    ret += colors[d.type](d.type) + ': ' + d.msg;
+    ret += this.colors[d.type](d.type) + ': ' + d.msg;
+    if (d.path && showContext && d.row && d.col) {
+      try {
+        let r0 = {
+          srow: d.row, scol: d.col,
+          erow: d.row, ecol: d.col + 1,
+        };
+        let ranges: Diagnostic.Range[] = [r0];
+        if (d.ranges) ranges.push(...d.ranges);
+        ranges = ranges.sort((a, b) => {
+          if (a.srow < b.srow)
+            return -1;
+          if (a.srow === b.srow) {
+            if (a.scol < b.scol)
+              return -1;
+            if (a.scol === b.scol && a.erow < b.erow || (a.erow === b.erow && a.ecol < b.ecol))
+              return -1;
+          }
+          return +1;
+        });
+        let contents = fs.readFileSync(d.path, { encoding: 'utf8' });
+        let line = 1;
+        let start_line_pos = 0, end_line_pos: number;
+        let print_pos = 1;
+        for (let i = 0; i < ranges.length; i++) {
+          let r = ranges[i];
+          let lines: string[] = [];
+          while (line < r.srow && (end_line_pos = contents.indexOf('\n', start_line_pos)) !== -1) {
+            line++;
+            print_pos = 1;
+            start_line_pos = end_line_pos + 1;
+          }
+          while (line < r.erow && (end_line_pos = contents.indexOf('\n', start_line_pos)) !== -1) {
+            lines.push(contents.substring(start_line_pos, end_line_pos).replace(/\r/g, ''));
+            line++;
+            start_line_pos = end_line_pos + 1;
+          }
+          end_line_pos = contents.indexOf('\n', start_line_pos);
+          if (end_line_pos === -1)
+            end_line_pos = contents.length;
+          lines.push(contents.substring(start_line_pos, end_line_pos).replace(/\r/g, ''));
+
+          if (print_pos === 1)
+            ret += "\n" + indent + lines[0] + "\n" + indent;
+          if (print_pos < r.scol)
+            ret += " ".repeat(r.scol - print_pos);
+          if (r.srow === r.erow) {
+            ret += chalk.green((r === r0 ? "^" : "~").repeat(r.ecol - Math.max(print_pos, r.scol)));
+            print_pos = r.ecol;
+          }
+          else {
+            ret += "~".repeat(lines[0].length + 1 - r.scol);
+            let last = lines.length - 1;
+            for (let l = 1, len = lines.length; l < len; i++) {
+              let line = lines[l];
+              ret += "\n" + indent + line + "\n" + indent;
+              ret += chalk.green("~".repeat(l === last ? r.ecol : line.length));
+            }
+            print_pos = r.ecol;
+          }
+        }
+      }
+      catch(e) { }
+    }
     if (d.notes && d.notes.length)
-      d.notes.forEach(d => ret += "\n" + ReporterPrinter.formatDiagnostic(d, indent + "  "));
+      d.notes.forEach(d => ret += "\n" + this.formatDiagnostic(d, indent + "  ", showContext));
     return ret;
   }
-  static formatReportConclusion(report: Report) {
+  formatReportConclusion(report: Report) {
     let ret = '';
-    ret += `${report.name} ${report.failed ? 'failed' : 'succeeded'}`;
-    if (report.diagnostics.length > 0)
+    ret += `${report.name} ${report.failed ? chalk.red('failed') : chalk.green('succeeded')}`;
+    if (report.diagnostics.length === 1)
+      ret += ` with ${report.diagnostics.length} issue`;
+    else if (report.diagnostics.length > 1)
       ret += ` with ${report.diagnostics.length} issues`;
     else if (!report.failed)
       ret += ` without issues`;
-    if (report.diagnostics.length > 0 && report.stats)
-      ret += ` (${ReporterPrinter.formatStats(report.stats)})`;
+    if (report.diagnostics.length > 0)
+      ret += ` (${this.formatStats()})`;
     if (report.duration !== undefined)
-      ret += ` (${util.Formatter.duration.millisecond.short(report.duration)})`;
+      ret += ` (took ${util.Formatter.duration.millisecond.short(report.duration)})`;
     return ret;
   }
-  static formatReportLogs(report: Report) {
+  formatReportLogs(report: Report, indentation: string) {
     let ret = '';
     if (report.diagnostics.length) {
-      ret += report.diagnostics.map(d => ReporterPrinter.formatDiagnostic(d)).join('\n');
-      if (report.failed && report.stats && report.stats.error === 0 && report.stats["fatal error"] === 0)
-        ret += "\n" + report.logs;
+      ret += report.diagnostics.map(d => this.formatDiagnostic(d, indentation)).join('\n');
+      if (report.failed && report.stats && report.stats.error === 0 && report.stats["fatal error"] === 0 && report.logs)
+        ret += "\n" + indent(indentation, report.logs);
     }
-    else if (report.failed) {
-      ret += report.logs;
+    else if (report.failed && report.logs) {
+      ret += indent(indentation, report.logs);
+    }
+    else {
+      ret += `${indentation}task failed for unknown reasons`;
     }
     return ret;
   }
-}
-
-export function mkReport(name: string, reporter: TaskReporter | Reporter, duration?: number) : Report {
-  return { name: name, logs: (reporter as TaskReporter).logs, diagnostics: reporter.diagnostics, failed: reporter.failed, duration: duration, stats: reduce(reporter.diagnostics, mkStats()) };
-}
-
-export function printReport(name: string, reporter: TaskReporter | Reporter, duration?: number) {
-  let report: Report = mkReport(name, reporter, duration);
-  if (report.failed || report.diagnostics.length)
-    console.log(embed('\n', ReporterPrinter.formatReportLogs(report), '\n'));
-  console.log(ReporterPrinter.formatReportConclusion(report));
-  return !reporter.failed;
 }
--- @msbuildsystem/cli/src/index.ts
+++ @msbuildsystem/cli/src/index.ts
@@ -1,8 +1,9 @@
 let boot_t0 = process.hrtime();
 import { Loader, Workspace, Reporter, util, Runner, RunnerContext, Async, Task, AttributePath, TaskDoMapReduce, Graph, Node } from '@openmicrostep/msbuildsystem.core';
-import { printReport, ReporterPrinter, mkReport, Report } from './common';
+import { ReporterPrinter, Report } from './common';
 import { args } from './args';
 import { npm } from './modules';
+import { stallDetector } from './stall';
 import * as chalk from 'chalk';
 import * as readline from 'readline';
 import * as tty from 'tty';
@@ -14,59 +15,6 @@ console.info("Boot time: ", (() => {
     return util.Formatter.duration.millisecond.short(ms);
 })());
 
-function throttle(ms: number, fn: () => void) : (() => void) & { now() : void } {
-  let last = Number.MIN_SAFE_INTEGER;
-  let timer: NodeJS.Timer | undefined = undefined;
-  function callnow() {
-    if (timer) {
-      clearTimeout(timer);
-      timer = undefined;
-    }
-    last = util.now();
-    fn();
-  }
-  return Object.assign(function throttled() {
-    let delta = util.now() - last;
-    if (delta >= ms)
-      callnow();
-    else if (!timer)
-      timer = setTimeout(callnow, ms - delta);
-  }, {
-    now: callnow
-  });
-}
-
-function stallDetector(limit: number) : { longest: number, last: number, mean(): number, stalls: number[], count: number, stop() : void } {
-  let ret = {
-    longest: 0,
-    last: 0,
-    mean() {
-      return ret.count > 0 ? ret.sum / ret.count : 0;
-    },
-    sum: 0,
-    count: 0,
-    stalls: [] as number[],
-    stop: stop
-  };
-  let t0 = util.now();
-  let immediate = setImmediate(function tick() {
-    let t = util.now();
-    let dt = t - t0;
-    ret.last = dt;
-    if (dt > limit)
-      ret.stalls.push(dt);
-    ret.longest = Math.max(ret.longest, dt);
-    ret.sum += dt;
-    ret.count++;
-    t0 = t;
-    immediate = setImmediate(tick);
-  });
-  function stop() {
-    clearImmediate(immediate);
-  }
-  return ret;
-}
-
 const cwd = process.cwd();
 if (args.workspace)
   args.workspace = util.pathJoinIfRelative(cwd, args.workspace);
@@ -75,7 +23,8 @@ if (args.command === "run" && args.action)
   args.command = args.action;
 //console.info("Arguments", args);
 
-(chalk as any).enabled = !!args.color;
+if (args.color === true)
+  (chalk as any).enabled = true;
 
 
 if (args.command === "modules") {
@@ -86,138 +35,150 @@ else {
 }
 
 function handle_run() {
-  let perf = util.performanceCounter("short");
+  let perf_load = util.performanceCounter("short");
   Loader.loadModules();
-  console.info("Modules [%s] loaded in %s", Array.from(Loader.modules.values()).map(m => m.name).join(', '), perf());
+  console.info("Modules [%s] loaded in %s", Array.from(Loader.modules.values()).map(m => m.name).join(', '), perf_load());
+  let perf = util.performanceCounter();
+  let printer = new ReporterPrinter();
   let workspace = new Workspace(args.workspace || undefined);
-  let results = true;
   let projects = args.projects.map(p => {
     let perf = util.performanceCounter();
     let project = workspace.project(p);
-    results = results && printReport(`Project ${p} load`, project.reporter, perf());
+    printer.push(new Report(`Project ${p}`, project.reporter, perf()));
     return project;
   });
   workspace.fixDirectoryPendingResolution();
-  results = results && printReport(`Workspace load`, workspace.reporter);
-  console.info(`Workspace: ${workspace.directory}`);
-  console.info(`Projects: ${[...workspace.projects.values()].map(p => `\n - ${p.path}`).join('')}`);
-  if (results) {
-    workspace.save();
-    let reporter = new Reporter();
-    let perf = util.performanceCounter();
-    let graph = workspace.buildGraph(reporter, {
-      environments: args.environments || undefined,
-      targets: args.targets || ([] as string[]).concat(...projects.map(p => p.targets.map(t => t.name))),
-      allowManual: !!args.targets
-    });
-    if (printReport('Build graph create', reporter, perf())) {
-      let options: any = {};
-      if (typeof args.jobs === "number")
-        options.maxConcurrentTasks = args.jobs;
-      if (typeof args.ide === "string")
-        options.ide = args.ide;
-      if (typeof args.full === "boolean")
-        options.full = args.full;
-      let runner = new Runner(graph, args.command, options);
-      let perf = util.performanceCounter();
-      let printer = new ReporterPrinter();
-      if (args.debug) {
-        function where(task: Node) {
-          return [...task.parents().reverse().slice(1), task].map(p => p.toString()).join(" - ");
-        }
-        runner.on("taskbegin", (context) => {
-          if (context.task instanceof Graph) return;
-          console.info("BEGIN  ", where(context.task));
-        });
-        runner.on("taskend", (context) => {
-          if (context.task instanceof Graph) return;
-          let wasrun = !!(context as any).actionRequired;
-          if (context.reporter.diagnostics.length) {
-            console.info("DIAGS ∨");
-            console.info(context.reporter.diagnostics.map(d => ReporterPrinter.formatDiagnostic(d)).join('\n'));
-            console.info("LOGS  ∨");
-            console.info(context.reporter.logs);
-            console.info(`END ${wasrun ? "X" : "O"} ∧`, where(context.task), (context.lastRunEndTime - context.lastRunStartTime) + 'ms');
-          }
-          else {
-            console.info(`END ${wasrun ? "X" : "O"}  `, where(context.task), (context.lastRunEndTime - context.lastRunStartTime) + 'ms');
-          }
-        });
-      }
-      runner.on("taskend", (context) => {
-        if (!(context.task instanceof Graph))
-          printer.push(mkReport(`${context.task.graph && context.task.target().__path()} ${context.task}`, context.reporter, context.lastRunEndTime - context.lastRunStartTime));
-      });
-      if (!args.debug && args.progress !== false && (args.progress === true || (process.stderr.isTTY && (process.stderr as tty.WriteStream).columns > 30))) {
-        let stderr = process.stderr as tty.WriteStream;
-        let tasks = new Set<Node>();
-        let nblines = 0;
-        let count = 0;
-        let run = 0;
-        let done = 0;
-        let stalls = stallDetector(100);
-        for (let task of runner.iterator(true))
-          if (!(task instanceof Graph))
-            ++count;
-        function writelines(lines: string[]) {
-          if (nblines > 0)
-            readline.moveCursor(stderr, 0, -nblines);
-          let i = 0;
-          for (; i < lines.length; i++) {
-            readline.clearLine(stderr, 0);
-            stderr.write(`\r${lines[i].substring(0, stderr.columns)}\n`);
-          }
-          nblines = lines.length;
-        }
+  printer.push(new Report(`Workspace ${workspace.directory}`, workspace.reporter));
+  if (printer.report.failed)
+    return end();
 
-        let progressbar = /*throttle(100, */(() => {
-          let progress1 = `   ${Math.floor(done * 100 / count)}`.slice(-3);
-          let progress2 = `(${run}/${done}/${count})`;
-          let width = Math.max(0, stderr.columns -  21 - progress2.length);
-          let partdone = Math.floor(done * width / count);
-          let stallsTxt = stalls.stalls.length === 0 ? 'none' : stalls.stalls.map(dt => util.Formatter.duration.millisecond.short(dt)).join(', ');
-          writelines([
-            `Progression: [${"-".repeat(partdone)}${" ".repeat(width - partdone)}] ${progress1}% ${progress2}`,
-            `Elapsed: ${util.Formatter.duration.millisecond.short(perf())}, Stalls: ${stallsTxt}, Issues: ${ReporterPrinter.formatStats(printer.report.stats) || "none"}`,
-            tasks.size > 0 ? `Active tasks (${tasks.size}): ${Array.from(tasks.values()).map(t => `${t.target()} > ${t}`).join(" & ")}` : `Done`,
-          ]);
-        });
-        runner.on("taskbegin", (context) => {
-          if (!(context.task instanceof Graph)) {
-            tasks.add(context.task);
-            progressbar();
-          }
-        });
-        runner.on("taskend", (context) => {
-          if (!(context.task instanceof Graph)) {
-            if ((context as any).actionRequired !== false)
-              ++run;
-            ++done;
-            tasks.delete(context.task);
-            progressbar();
-          }
-          if (context.task === graph) {
-            progressbar/*.now*/();
-            stalls.stop();
-          }
-        });
+  workspace.save();
+  let reporter = new Reporter();
+  let perf_grap = util.performanceCounter();
+  let graph = workspace.buildGraph(reporter, {
+    environments: args.environments || undefined,
+    targets: args.targets || ([] as string[]).concat(...projects.map(p => p.targets.map(t => t.name))),
+    allowManual: !!args.targets
+  });
+  printer.push(new Report('Build graph generation', reporter, perf_grap()));
+  if (printer.report.failed)
+    return end();
+
+  let options: any = {};
+  if (typeof args.jobs === "number")
+    options.maxConcurrentTasks = args.jobs;
+  if (typeof args.ide === "string")
+    options.ide = args.ide;
+  if (typeof args.full === "boolean")
+    options.full = args.full;
+  let runner = new Runner(graph, args.command, options);
+  if (args.debug) {
+    function where(task: Node) {
+      return [...task.parents().reverse().slice(1), task].map(p => p.toString()).join(" - ");
+    }
+    runner.on("taskbegin", (context) => {
+      if (context.task instanceof Graph) return;
+      console.info("BEGIN  ", where(context.task));
+    });
+    runner.on("taskend", (context) => {
+      if (context.task instanceof Graph) return;
+      let wasrun = !!(context as any).actionRequired;
+      if (context.reporter.diagnostics.length) {
+        console.info("DIAGS ∨");
+        console.info(context.reporter.diagnostics.map(d => printer.formatDiagnostic(d)).join('\n'));
+        console.info("LOGS  ∨");
+        console.info(context.reporter.logs);
+        console.info(`END ${wasrun ? "X" : "O"} ∧`, where(context.task), (context.lastRunEndTime - context.lastRunStartTime) + 'ms');
       }
-      let provider: TaskDoMapReduce<any, any> | undefined = undefined;
-      let ok = true;
-      if (args.command === "generate") {
-        let reporter = new Reporter();
-        provider = Task.generators.validate.validate(reporter, new AttributePath('ide'), args.ide);
-        ok = !!(provider && printReport('Find generator', reporter));
+      else {
+        console.info(`END ${wasrun ? "X" : "O"}  `, where(context.task), (context.lastRunEndTime - context.lastRunStartTime) + 'ms');
       }
-      if (ok) {
-        Async.run<RunnerContext>(null, [
-          f => provider ? runner.runWithMapReduce(f, provider) : runner.run(f),
-          (p) => {
-            console.log(printer.formatReports('Build', perf()));
-            process.exit(p.context.failed ? 4 : 0);
-          }
-        ]);
-      }  else process.exit(3);
-    } else process.exit(2);
-  } else process.exit(1);
+    });
+  }
+  runner.on("taskend", (context) => {
+    if (!(context.task instanceof Graph))
+      printer.push(new Report(
+        `Target ${context.task.graph && context.task.target().__path()}, task ${context.task}`,
+        context.reporter,
+        context.lastRunEndTime - context.lastRunStartTime
+      ));
+  });
+  if (!args.debug && args.progress !== false && (args.progress === true || (process.stderr.isTTY && (process.stderr as tty.WriteStream).columns > 30))) {
+    let stderr = process.stderr as tty.WriteStream;
+    let tasks = new Set<Node>();
+    let nblines = 0;
+    let count = 0;
+    let run = 0;
+    let done = 0;
+    let stalls = stallDetector(100);
+    let averageTaskLoad = 0;
+    for (let task of runner.iterator(true))
+      if (!(task instanceof Graph))
+        ++count;
+    function writelines(lines: string[]) {
+      if (nblines > 0)
+        readline.moveCursor(stderr, 0, -nblines);
+      let i = 0;
+      for (; i < lines.length; i++) {
+        readline.clearLine(stderr, 0);
+        stderr.write(`\r${lines[i].substring(0, stderr.columns)}\n`);
+      }
+      nblines = lines.length;
+    }
+
+    let progressbar = /*throttle(100, */(() => {
+      let progress1 = `   ${Math.floor(done * 100 / count)}`.slice(-3);
+      let progress2 = `(${run}/${done}/${count})`;
+      let width = Math.max(0, stderr.columns -  21 - progress2.length);
+      let partdone = Math.floor(done * width / count);
+      let stallsTxt = stalls.stalls.length === 0 ? 'none' : stalls.stalls.map(dt => util.Formatter.duration.millisecond.short(dt)).join(', ');
+      writelines([
+        `Progression: [${"-".repeat(partdone)}${" ".repeat(width - partdone)}] ${progress1}% ${progress2}`,
+        `Elapsed: ${util.Formatter.duration.millisecond.short(perf())}, ` +
+        `Load: ${averageTaskLoad}, ` +
+        `Issues: ${printer.formatStats() || "none"}`,
+        tasks.size > 0 ? `Active tasks (${tasks.size}): ${Array.from(tasks.values()).map(t => `${t.target()} > ${t}`).join(" & ")}` : `Done`,
+        `Stalls: ${stallsTxt}`,
+      ]);
+    });
+    runner.on("taskbegin", (context) => {
+      if (!(context.task instanceof Graph)) {
+        tasks.add(context.task);
+        progressbar();
+      }
+    });
+    runner.on("taskend", (context) => {
+      if (!(context.task instanceof Graph)) {
+        if ((context as any).actionRequired !== false)
+          ++run;
+        ++done;
+        tasks.delete(context.task);
+        progressbar();
+      }
+      if (context.task === graph) {
+        progressbar/*.now*/();
+        stalls.stop();
+      }
+    });
+  }
+  let provider: TaskDoMapReduce<any, any> | undefined = undefined;
+  if (args.command === "generate") {
+    let reporter = new Reporter();
+    provider = Task.generators.validate.validate(reporter, new AttributePath('ide'), args.ide);
+    printer.push(new Report(`Generator validation`, reporter));
+  }
+  if (printer.report.failed)
+    return end();
+  Async.run<RunnerContext>(null, [
+    f => provider ? runner.runWithMapReduce(f, provider) : runner.run(f),
+    f => {
+      end();
+      f.continue();
+    }
+  ]);
+
+  function end() {
+    console.log(printer.formatReports('Build', perf()));
+    process.exit(printer.report.stats['error'] + printer.report.stats['fatal error']);
+  }
 }
